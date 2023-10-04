17f8c98cf2afceb1c215ae8931ba8540af607f99
Logan Pendergraft <ldpendergraft@gmail.com>
2023-10-04 09:27:04 -0500

    Changed Port for Server

--- database/server/server.js
+++ database/server/server.js
@@ -2,8 +2,8 @@ var express  = require("express");
 
 var app = express();
 
-app.listen(3000,()=>{
-    console.log("Server started on port 3000");
+app.listen(2260,()=>{
+    console.log("Server started on port 2260");
 });
 
 app.get("/",(req, res)=>{
